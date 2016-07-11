00191add676256f06b00e51811620c9dc308c542
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-07-11 22:15:24 +0100

    bump

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 if [ "$1" == 'stop' ]; then
         echo "Stopping selenium server..."
         pid=`ps -eo pid,args | grep server-standalone-2.47.0.jar | grep -v grep | cut -c1-6`
-        kill -9 $pid
+        echo $pid
+        kill $pid
 elif [ "$1" == 'start' ]; then
         echo "Starting selenium server..."
         PATH="./node_modules/phantomjs/bin:$PATH" java -jar selenium-server-standalone-2.47.0.jar &> /dev/null &
