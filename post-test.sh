13769e5118442a7d909accbf17d2d1a2f93807bb
Sabeur Thabti <thabti.sabeur@gmail.com>
2016-07-15 09:30:44 +0100

    chrome driver

--- post-test.sh
+++
@@ -1,1 +0,0 @@
-pid=`ps -eo pid,args | grep server-standalone-2.47.0.jar | grep -v grep | cut -c1-6`
--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
 
   capabilities: [
     {
-      browserName: 'phantomjs'
+      browserName: 'chrome'
     }
   ],
 
